9636011a6220e8526b1679f7176aacfef042b57a
javierSolanoP <jmsolano24@misena.edu.co>
2021-08-28 18:30:21 -0500

    Se añade las funcionalidades para validar los datos recibidos.

--- app/Http/Controllers/Admin/Class/Administrador.php
+++ app/Http/Controllers/Admin/Class/Administrador.php
@@ -32,6 +32,123 @@ class Administrador extends User{
         }
     }
 
+    //Metodos que realiza un usuario: 
     use MethodsUser;
+
+    //Metodo para validar una categoria de producto: 
+    public function validateCategory()
+    {
+
+        if($_SESSION['category']){
+
+            $data = $_SESSION['category'];
+            $validate = [];
+
+            if(!empty($data->nombre) && !preg_match("/[0-9]/", $data->nombre)){
+
+                $validate['nombre'] = true;
+
+            }else{
+
+                $response = ['Error' => 'Categoria vacia o con tipo de dato no permitido.'];
+                die($response);
+
+            }
+
+        }
+
+    }
+
+    //Metodo para validar un producto: 
+    public function validateProduct()
+    {
+
+        if($_SESSION['product']){
+
+            $data = $_SESSION['product'];
+            $validate = [];
+
+            //Validar nombre de producto: 
+            if(!empty($data->nombre) && !preg_match("/[0-9]/", $data->nombre)){
+
+                $validate['nombre'] = true;
+
+            }else{
+
+                $response = ['Error' => 'Campo nombre: vacio o tipo de dato no permitido.'];
+                die($response);
+
+            }
+
+            //Validar descripcion de producto: 
+            if(!empty($data->descripcion && !preg_match("/[0-9]/", $data->descripcion))){
+
+                $validate['descripcion'] = true;
+
+            }else{
+
+                $response = ['Error' => 'Campo descripcion: vacio o tipo de dato no permitido.'];
+                die($response);
+
+            }
+
+            //Validar marca del producto: 
+            if(!empty($data->marca && !preg_match("/[0-9]/", $data->marca))){
+
+                $validate['marca'] = true;
+
+            }else{
+
+                $response = ['Error' => 'Campo marca: vacio o tipo de dato no permitido.'];
+                die($response);
+
+            }
+
+            //Validar precio del producto: 
+            if(!empty($data->precio) && preg_match("/[0-9]/", $data->precio)){
+
+                if(is_string($data->precio) || is_integer($data->precio) || is_float($data->precio)){
+
+                    $validate['precio'] = doubleval($data->precio);
+
+                }
+
+            }else{
+
+                $response = ['Error' => 'Campo precio: vacio o tipo de dato no permitido.'];
+                die($response);
+
+            }
+
+            //Validar cantidades del producto: 
+            if(!empty($data->cantidad) && preg_match("/[0-9]/", $data->cantidad)){
+
+                if(is_string($data->cantidad) || is_double($data->cantidad) || is_float($data->cantidad)){
+
+                    $validate['cantidad'] = intval($data->cantidad);
+
+                }
+
+            }else{
+
+                $response = ['Error' => 'Campo cantidad: vacio o tipo de dato no permitido.'];
+                die($response);
+
+            }
+
+            $response = ['validate' => true, 'fields' => $validate];
+            return $response;
+
+        }else{
+
+            $response = ['validate' => false, 'Error' => 'No existen datos.'];
+            return $response;
+
+        }
+
+    }
+
+    //Metodos para realizar una conexion entre modulos: 
     use MethodsConnect;  
+    
 }
